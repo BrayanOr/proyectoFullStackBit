3a78e8f89b127b00859dc1fec130d325f7617852
Brayan O <brayanramirez018@gmail.com>
2025-06-24 20:49:04 -0500

    First commit

---
+++ back/src/index.js
@@ -0,0 +1,4 @@
+import servidor from "./servidor";
+servidor.listen(3000, () => {
+    console.log("El servidor esta escuchado en el link http://localhost:3000");
+})
---
+++ back/src/servidor.js
@@ -0,0 +1,10 @@
+import express from "express"; // para realizar la conexion con el servidor
+import morgan from "morgan"; //monitrear solicitudes http
+
+const servidor = express();
+servidor.use(morgan("dev"));
+servidor.use(express.json());
+
+servidor.get('/' , (sol, res) => {
+    res.status(404)
+})
